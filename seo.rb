efe46170dfc62d3dbaafe2ca8d0e5b2747319233
Gerald Ke <gerald@canva.com>
2020-04-28 11:28:44 +0800

    Adds outlinks

--- seo.rb
+++ seo.rb
@@ -24,28 +24,53 @@ target_host = input if !input.to_s.strip.empty?
 page1 = RestClient.get(base_host + path, cookies: { proxyKey: proxy_key })
 
 doc = Nokogiri::HTML.parse(page1)
+
 nodeset = doc.xpath('//link[@hreflang]/@href')
 nodeset.map { |element| element["href"] }.compact
-
 hreflangs1 = nodeset.to_a.map(&:to_s)
+
+nodeset = doc.xpath('//a[@href]/@href')
+nodeset.map { |element| element["href"] }.compact
+outlinks1 = nodeset.to_a.map(&:to_s)
 
 page2 = RestClient.get(target_host + path, cookies: { proxyKey: proxy_key })
 
 doc = Nokogiri::HTML.parse(page2)
+
 nodeset = doc.xpath('//link[@hreflang]/@href')
 nodeset.map { |element| element["href"] }.compact
-
 hreflangs2 = nodeset.to_a.map(&:to_s)
+
+nodeset = doc.xpath('//a[@href]/@href')
+nodeset.map { |element| element["href"] }.compact
+outlinks2 = nodeset.to_a.map(&:to_s)
 
 hreflangs2.map! do |hreflang|
     hreflang.sub(target_host.sub(auth, ''), base_host)
 end
 
-additional = hreflangs2 - hreflangs1
-missing = hreflangs1 - hreflangs2
+outlinks2.map! do |outlink|
+    outlink.sub(target_host.sub(auth, ''), base_host)
+end
+
+additional_hreflangs = hreflangs2 - hreflangs1
+missing_hreflangs = hreflangs1 - hreflangs2
+
+additional_outlinks = outlinks2 - outlinks1
+missing_outlinks = outlinks1 - outlinks2
+
+puts "HREFLANGS:"
 
 puts "Additional:"
-ap additional
+ap additional_hreflangs
 
 puts "Missing:"
-ap missing
+ap missing_hreflangs
+
+puts "OUTLINKS:"
+
+puts "Additional:"
+ap additional_outlinks
+
+puts "Missing:"
+ap missing_outlinks
